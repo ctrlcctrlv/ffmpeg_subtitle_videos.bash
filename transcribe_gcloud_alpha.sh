b1da5b913e0554c346fad442778824996f3f88b0
Fred Brennan <copypaste@kittens.ph>
2023-01-30 15:52:51 -0500

    Improve transcribe_gcloud_alpha.sh

--- transcribe_gcloud_alpha.sh
+++ transcribe_gcloud_alpha.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 transcribe_gcloud_alpha() {
-	gcloud alpha storage cp "$1" gs://transcribe_random_oneoffs/ &&
-	gcloud alpha ml speech recognize-long-running --enable-automatic-punctuation --include-word-time-offsets --model=video_enhanced --encoding=flac --language-code=en-US --sample-rate=48000 --enable-speaker-diarization "gs://transcribe_random_oneoffs/$1" > "$2"
+	TYPE="${1##*.}"
+	FILE="$1"
+	[[ "$TYPE" == "opus" || "$TYPE" == "flac" ]] && : || \
+		(ffmpeg -i "$FILE" -ac 1 -f opus "${1%%.*}.opus" && \
+			FILE="${1%%.*}.opus")
+	TYPE="${1##*.}"
+	[[ "$TYPE" == "opus" ]] && TYPE=ogg-opus 
+	FILEU=$(jq -Rr "@uri" <<< "$FILE")
+	>&2 echo "File is $FILE ($FILEU)."
+	(gcloud alpha storage cp <(cat "$FILE") "gs://transcribe_random_oneoffs/$FILEU") &&
+	gcloud alpha ml speech recognize-long-running --encoding="$TYPE" --sample-rate=48000 --enable-automatic-punctuation --include-word-time-offsets --model=video_enhanced --language-code=en-US --enable-speaker-diarization "gs://transcribe_random_oneoffs/$FILEU" $3 > "$2"
 }
 
-transcribe_gcloud_alpha $@
+transcribe_gcloud_alpha "$@"
+
